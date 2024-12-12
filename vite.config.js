6f06e6006cdd1e6aa0ca3e42aefd22deda1ab2f5
Emadenni <emanyele.dng86@gmail.com>
2024-12-12 14:11:09 +0100

    debugging coverage configuration

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,  
-    environment: "jsdom",  
-    setupFiles: "./setup-tests.js",  
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setup-tests.js",
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.{ts,tsx,js,jsx}'],
-      outputDirectory: './coverage', 
+      provider: 'v8', 
+      reporter: ['text', 'json', 'html'],  
+      include: ['src/**/*.{ts,tsx,js,jsx}'], 
+      outputDirectory: './coverage',  
+      all: true, 
     },
   },
 });
