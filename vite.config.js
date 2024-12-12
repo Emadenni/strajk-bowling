cc2bd6445336591dac9b074d59b51c25814934b1
Emadenni <emanyele.dng86@gmail.com>
2024-12-12 13:44:11 +0100

    added coverage configuration

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,10 @@ export default defineConfig({
     globals: true,  
     environment: "jsdom",  
     setupFiles: "./setup-tests.js",  
+    coverage: {
+      provider: 'v8',  
+      reporter: ['text', 'json', 'html'], 
+      include: ['src/**/*.ts', 'src/**/*.tsx'], 
+    },
   },
 });
